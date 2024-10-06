67836a14cb11dee05f3f570792b02f3882b4cc5b
decoded-cipher <arjun12345krishna@gmail.com>
2024-10-06 07:27:48 +0530

    fix : Remove Blank space

--- server/helpers/gemini.js
+++ server/helpers/gemini.js
@@ -7,15 +7,6 @@ const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-// async function run(message) {
-//   const prompt = `Talk me as Mars${message}`;
-//   const result = await model.generateContent(prompt);
-//   const response = await result.response;
-//   const text = await response.text();
-//   return text;
-// }
-// module.exports = run;
-
 async function run(message, planetInfo, previousContent) {
   const prompt = `Talk to me as ${planetInfo.name}: ${message} - ${previousContent}`;
 
@@ -31,7 +22,9 @@ async function run(message, planetInfo, previousContent) {
   // Split the text into an array of lines
   const lines = text.split("\n");
 
-  return lines;
+  // remove empty lines
+  return lines.filter((line) => line.trim() !== "");
+
 }
 
 module.exports = run;
