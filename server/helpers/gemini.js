fa3d01e99643ebe8693a06956610879212a886fe
Badhusha <badhusha.shaji22@gmail.com>
2024-10-06 10:45:36 +0530

    prompt update

--- server/helpers/gemini.js
+++ server/helpers/gemini.js
@@ -8,8 +8,11 @@ const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 async function run(message, planetInfo, previousContent) {
-  const prompt = `Talk to me as ${planetInfo.name}: ${message} - Use the chat memory to remember our previous conversation: ${previousContent}`;
-
+  // console.log(JSON.stringify(previousContent));
+  
+  const prompt = `Use the chat memory to remember our previous conversation: ${JSON.stringify(previousContent)} these are our old conversation you have to answer according to these msg take the as uer mamory , if you want you can take this as the memory of your self ${JSON.stringify(planetInfo)}, now  think you are ${planetInfo.name} planet now talk to me as ${planetInfo.name} , this is my question  : ${message} `;
+  // console.log(prompt);
+  
   // Generate content based on the prompt
   const result = await model.generateContent(prompt);
 
